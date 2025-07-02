bfc6e2667dfc41fd13817c49b86004afdce8bfcd
Sylvee <79902941+notsylvee@users.noreply.github.com>
2025-07-02 19:30:05 -0400

    more voicelines

--- src/events/voiceline.js
+++ src/events/voiceline.js
@@ -16,14 +16,30 @@ module.exports = {
             "That isn't yours.",
             "I think there's something beneath your skin. you should pry it out, kinda gross..",
             "Hi~",
-            "I can see your organs..",
+            "I can see your organs.. *gross*.",
             "AAAAHH!!",
             "BOO!",
             "Did I scare you?",
             "Why are you so rude?",
             "You aren't a good friend.",
             "Hey.",
-            "How big is this place?"
+            "How big is this place?",
+            "I see your organs. I think you should go see a doctor or something.",
+            "Do you know yourself? I’d like to know myself.",
+            "Yeah, this doesn’t feel safe.",
+            "Don’t do that.",
+            "You don’t own that.",
+            "Put that down. It’s not nice to take stuff without asking.",
+            "We should go somewhere else.",
+            "You’re weird.",
+            "Why are you so quiet?",
+            "You don’t own those. Y-you should return it, I think.",
+            "Do you own this place? Pretty big building for someone as small as you.",
+            "WATCH OUT!!",
+            "Can we go over there?",
+            "What will happen when time runs out? Eh, probably best not to think about it.",
+            "The air reeks of death.",
+            "Why are you following me?"
         ];
         const voiceLine = voiceLines[Math.floor(Math.random() * voiceLines.length)];
         if (chance < 249) {
